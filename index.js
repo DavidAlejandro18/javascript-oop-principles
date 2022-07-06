c524c3463b66e169c57528aab214ba29e4c27fb5
David Tovar <alextovar146@gmail.com>
2022-07-05 19:08:44 -0500

    Almost everything is an Object

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(typeof "hello world");
+console.log(typeof 100);
+console.log(typeof true);
+
+console.log(typeof {});
+console.log(typeof []);
+
+console.log(typeof {});
+console.log(typeof []);
+
+console.log(typeof new Date());
+console.log(typeof new RegExp());
+
+console.log(new Number(100));
+console.log(new String("hello world"));
+console.log(new Boolean(true));

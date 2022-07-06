94a06ff978f74cdd4e797568c6c8e198663788f8
David Tovar <alextovar146@gmail.com>
2022-07-05 19:20:31 -0500

    Object literal

---
+++ object-literal.js
@@ -0,0 +1,19 @@
+console.log({});
+
+let name = "David";
+let lastname = "Tovar";
+let age = 21;
+
+const user = {
+    name: "David",
+    lastname: "Tovar",
+    age: 21,
+    hobbies: ['video games', 'movies', 'music'],
+    adress: {
+        city: "Monterrey",
+        state: "Nuevo León",
+        country: "Mexico"
+    }
+}
+
+console.log(user);

c561c0e2f6a2d804adda5bca43ff2cf35aa68c08
David Tovar <alextovar146@gmail.com>
2022-07-05 19:30:38 -0500

    Properties

---
+++ properties.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "David", // properties
+    lastname: "Tovar", // properties
+    age: 21, // properties
+    hobbies: ['video games', 'movies', 'music'], // properties
+    adress: {
+        city: "Monterrey",
+        state: "Nuevo León",
+        country: "Mexico"
+    }
+}
+
+console.log(user);
